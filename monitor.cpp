99fe84e7cce3ed055010e8422486793f5ba99474
Vivek Antony <Vivek.Antony@philips.com>
2024-09-12 14:50:37 +0530

    changes

--- monitor.cpp
+++ monitor.cpp
@@ -4,6 +4,14 @@
 #include <chrono>
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+using namespace std;
+
+string valueIsWithinRange = "NORMAL";
+string upperLimitWarning = "TEMP_HYPER_WARNING";
+string lowerLimitWarning = "TEMP_HYPO_WARNING";
+string temperatureCritical = "TEMP_CRITICAL";
+string pulseRateCritical = "PULSERATE_CRITICAL";
+string spo2Critical = "SPO2_CRITICAL";
 
 void displayMessage(std::string message) {
     cout << message << "\n";
@@ -15,32 +23,61 @@ void displayMessage(std::string message) {
     }
 }
 
-bool isTemperatureCritical(float temperature) {
-    if (temperature > 102 || temperature < 95) {
-        displayMessage("Temperature is critical!");
-    return 0;
-    }
-return 1;
+bool isParamsNotInRange(double value, double lowerLimit, double upperLimit) {
+    return (value < lowerLimit) || (value > upperLimit);
 }
 
+void checkEarlyWarning(float currentvalue, float upperLimit, float lowerLimit) {
+    float tolerance = 0.015f;
+    std::string message;
+    float upperWarningLimit = upperLimit - (upperLimit * tolerance);
+    float lowerWarningLimit = lowerLimit + (lowerLimit * tolerance);
+    if (currentvalue > upperWarningLimit)
+        message = upperLimitWarning;
+    else if (currentvalue < lowerWarningLimit)
+        message = lowerLimitWarning;
+    else
+        message = valueIsWithinRange;
+    displayMessage(message);
+}
 
-bool isPulseRateOutOfRange(float pulseRate) {
-    if (pulseRate < 60 || pulseRate > 100) {
-        displayMessage("Pulse Rate is out of range!");
+int checkTemperatureValue(float temperature) {
+    bool checkTemperatureNotInRange = isParamsNotInRange(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT);
+    if (checkTemperatureNotInRange) {
+        displayMessage("TEMP_CRITICAL");
         return 0;
     }
-    return 1;
+    else {
+        checkEarlyWarning(temperature, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_LOWER_LIMIT);
+        return 1;
+    }
 }
 
-bool isSpo2OutOfRange(float spo2) {
-    if (spo2 < 90) {
-        displayMessage("Oxygen Saturation out of range!");
+int checkPulseRateValue(float pulseRate) {
+    bool checkPulseRateNotInRange = isParamsNotInRange(pulseRate, PULSE_RATE_LOWER_LIMIT, PULSE_RATE_UPPER_LIMIT);
+    if (checkPulseRateNotInRange) {
+        displayMessage(pulseRateCritical);
         return 0;
     }
-    return 1;
+    else {
+        checkEarlyWarning(pulseRate, PULSE_RATE_UPPER_LIMIT, PULSE_RATE_LOWER_LIMIT);
+        return 1;
+    }
 }
 
-int vitalsOk(float temp, float pulse, float spo2) {
-    return (isTemperatureCritical(temp) && isPulseRateOutOfRange(pulse) && isSpo2OutOfRange(spo2));
+int checkSpo2Value(float spo2) {
+    bool checkSpo2NotInRange = isParamsNotInRange(spo2, SPO2_LOWER_LIMIT, SPO2_UPPER_LIMIT);
+    if (checkSpo2NotInRange) {
+        displayMessage(spo2Critical);
+        return 0;
+    }
+    else {
+        checkEarlyWarning(spo2, SPO2_UPPER_LIMIT, SPO2_LOWER_LIMIT);
+        return 1;
+    }
+}
+
+int areVitalsNormal(float temp, float pulse, float spo2) {
+    return (checkTemperatureValue(temp) && checkPulseRateValue(pulse) && checkSpo2Value(spo2));
 }
 
--- monitor.h
+++ monitor.h
@@ -1,3 +1,10 @@
 #pragma once
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+const double TEMPERATURE_LOWER_LIMIT = 95.0;
+const double TEMPERATURE_UPPER_LIMIT = 102.0;
+const double PULSE_RATE_LOWER_LIMIT = 60.0;
+const double PULSE_RATE_UPPER_LIMIT = 100.0;
+const double SPO2_LOWER_LIMIT = 90.0;
+const double SPO2_UPPER_LIMIT = 100.0;
+
+int areVitalsNormal(float temperature, float pulseRate, float spo2);
